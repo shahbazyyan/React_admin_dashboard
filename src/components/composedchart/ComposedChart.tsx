9ec1fbd8804f8b831119c362c68add4978b50021
David Shahbazyan Mushegh <david.shahbazyan.m@totogaming.am>
2023-11-23 11:02:59 +0400

    Created big chart and complated all Home page

--- src/components/composedchart/ComposedChart.tsx
+++ src/components/composedchart/ComposedChart.tsx
@@ -1,9 +1,77 @@
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import './composedchart.scss';
+
+const data = [
+    {
+      name: "Sun",
+      economic: 8425,
+      clothes: 2489,
+      electronic: 8900,
+    },
+    {
+      name: "Mon",
+      economic: 4563,
+      clothes: 8941,
+      electronic: 2910,
+    },
+    {
+      name: "Tue",
+      economic: 3060,
+      clothes: 9880,
+      electronic: 5290,
+    },
+    {
+      name: "Wed",
+      economic: 4630,
+      clothes: 6408,
+      electronic: 2099,
+    },
+    {
+      name: "Thu",
+      economic: 9890,
+      clothes: 9650,
+      electronic: 4581,
+    },
+    {
+      name: "Fri",
+      economic: 8545,
+      clothes: 2356,
+      electronic: 4523,
+    },
+    {
+      name: "Sat",
+      economic: 8564,
+      clothes: 2624,
+      electronic: 8562,
+    },
+  ];
 
 function ComposedChart() {
   return (
     <div className='composedchart'>
-        
+        <h1>Revenue Analytics</h1>
+        <div className='chart'>
+        <ResponsiveContainer width="99%" height="100%">
+        <AreaChart
+          data={data}
+          stackOffset="expand"
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis/>
+          <Tooltip/>
+          <Area type="monotone" dataKey="electronic" stackId="1" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="clothes" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+          <Area type="monotone" dataKey="economic" stackId="1" stroke="#ffc658" fill="#ffc658" />
+        </AreaChart>
+      </ResponsiveContainer>
+      </div>
     </div>
   )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import PieChartBox from '../../components/PieChartBox/PieChartBox';
 import Barchartbox from '../../components/barchartbox/Barchartbox';
 import Chartbox from '../../components/chartbox/Chartbox';
+import ComposedChart from '../../components/composedchart/ComposedChart';
 import Topbox from '../../components/topbox/Topbox';
 import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data';
 import './home.scss';
@@ -16,7 +17,7 @@ function Home() {
         <div className="box box4"><PieChartBox/></div>
         <div className="box box5"><Chartbox {...chartBoxProduct}/></div>
         <div className="box box6"><Chartbox {...chartBoxConversion}/></div>
-        <div className="box box7"></div>
+        <div className="box box7"><ComposedChart/></div>
         <div className="box box8"><Barchartbox {...barChartBoxVisit}/></div>
         <div className="box box9"><Barchartbox {...barChartBoxRevenue}/></div>
     </div>
